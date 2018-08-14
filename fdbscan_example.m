b259be3190923c5f42b55b385a11663e524ee8ae
Rolf office <r.harkes@nki.nl>
2018-08-14 09:32:32 +0200

    update example to process 30k localizations.

--- fdbscan_example.m
+++ fdbscan_example.m
@@ -1,20 +1,19 @@
 %generate data
-rndpts = 1E3;
+rndpts = 1E4;
 cluster = 40;
-pt_cluster = 50;
+pt_cluster = 500;
 d_cluster = repmat(0.005,[1,cluster]);
 xy = nan(rndpts+cluster*pt_cluster,2);
 xy(1:rndpts,:)=rand(rndpts,2);
 for ct = 1:cluster
     start = rndpts+1+(ct-1)*pt_cluster;
     finish = start+pt_cluster-1;
-    ct
-    loc = rand(1,2)
+    loc = rand(1,2);
     xy(start:finish,:)=randn(pt_cluster,2)*d_cluster(ct)+loc;
 end
 
 %fdbscan
-tic;class=fdbscan(xy,10,0.0065);toc
+tic;class=fdbscan(xy,10,0.0035);toc
 figure(1);clf;hold on;
 Ncl = max(class);
 pts=nan(1,Ncl);
@@ -23,5 +22,5 @@ for ct = 1:Ncl
     pts(ct) = sum(class==ct);
 end
 plot(xy(class==0,1),xy(class==0,2),'k.');
-axis equal
+axis([0 1 0 1])
 title(sprintf('found %d clusters with on averages %.2f points',Ncl,mean(pts)))
